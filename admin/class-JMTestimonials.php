4e630d3c60f31d0e216c64383f0bf6e2022ad028
jasonmitchell42 <2016jasonmitchell@gmail.com>
2020-07-19 22:32:42 +1000

    Initial commit for new plugin - Added file structure

---
+++ admin/class-JMTestimonials.php
@@ -0,0 +1,27 @@
+<?php
+if ( ! class_exists( 'JMTestimonialsAdmin' ) ) {
+    class JMTestimonialsAdmin {
+        public function __construct()
+        {
+            // todo: add actions for the custom post type
+        }
+
+        public function create_post_type()
+        {
+            // todo: create post type
+        }
+
+        public function columns_head($defaults)
+        {
+            // todo: add custom values to post columns
+        }
+
+        public function columns_content($defaults)
+        {
+            // todo: match custom columns with content
+        }
+
+    }
+    $jm_testimonials = new JMTestimonialsAdmin;
+}
+
---
+++ testimonials.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name:  Custom Testimonials Plugin
+Description:  A simple testimonials plugin
+Version:      1.0
+Author:       Jason Mitchell
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  jm-testimonials
+Domain Path:  /languages
+ */
+
+if (!class_exists('JMTestimonials')) {
+    class JMTestimonials
+    {
+        public function __construct()
+        {
+        }
+
+        public function enqueue_scripts()
+        {
+        }
+
+    }
+}
